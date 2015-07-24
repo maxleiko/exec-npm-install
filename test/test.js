c97e6d5e82b24c40df87222772c1c92288b6f427
Maxime Tricoire <max.tricoire@gmail.com>
2015-07-24 13:44:02 +0200

    update test so that they are mocha-able

--- test/test.js
+++ test/test.js
@@ -1,18 +1,25 @@
 var npmInstall = require('../npm-install');
 
-var options = {
-    modules: [
-        'kevoree-node-javascript',
-        'kevoree-comp-ticker',
-        'kevoree-group-ws'
-    ],
-    prefix: '.deploy_units'
-};
+describe('Dummy test', function () {
+    this.timeout(10000);
 
-npmInstall(options, function (err) {
-    if (err) {
-        console.log('Error: '+err.message);
-    } else {
-        console.log('All set');
-    }
+    var options = {
+        modules: [
+            'kevoree-node-javascript',
+            'kevoree-comp-ticker',
+            'kevoree-group-ws'
+        ],
+        prefix: '.deploy_units'
+    };
+
+    it('should install 3 modules', function (done) {
+        npmInstall(options, function (err) {
+            if (err) {
+                console.log('Error: '+err.message);
+            } else {
+                console.log('All set');
+            }
+            done(err);
+        });
+    });
 });
